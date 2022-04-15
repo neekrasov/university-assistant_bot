ea22ba405e52086195539aa2c5291a7daa14ef85
neekrasov <neekrasov@gmail.com>
2022-04-15 23:03:18 +0300

    add private filter to handlers

--- tg_bot/handlers/users/base_handler.py
+++ tg_bot/handlers/users/base_handler.py
@@ -2,15 +2,16 @@ from aiogram import types
 from aiogram.dispatcher.filters import CommandHelp
 from aiogram.dispatcher.filters.builtin import CommandStart
 
+from filters.private import IsPrivate
 from loader import dp
 
 
-@dp.message_handler(CommandStart())
+@dp.message_handler(IsPrivate(), CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Привет, {message.from_user.full_name}!")
 
 
-@dp.message_handler(CommandHelp())
+@dp.message_handler(IsPrivate(), CommandHelp())
 async def bot_help(message: types.Message):
     text = ("Список команд: ",
             "/start - Начать диалог",
